93d31b655f9d0be7bcf5461bc201f7f8969e273f
ayobyary <140202386+ayobyary@users.noreply.github.com>
2025-11-03 22:22:17 +0330

    cofe-club

---
+++ src/app/Components/coffee-club/coffee-club.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee-club',
+  standalone: true,
+  templateUrl: './coffee-club.component.html',
+})
+export class CoffeeClubComponent {}
+

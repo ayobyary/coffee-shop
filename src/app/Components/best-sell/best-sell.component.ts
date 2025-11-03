e88ec6999c2cc052a5777f256ca94a488ed739a7
ayobyary <140202386+ayobyary@users.noreply.github.com>
2025-11-03 21:00:44 +0330

    refactor: extract best-sell section into standalone component (app-best-sell) and replace in all.component.html

---
+++ src/app/Components/best-sell/best-sell.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-best-sell',
+  standalone: true,
+  templateUrl: './best-sell.component.html',
+})
+export class BestSellComponent {}
+
+

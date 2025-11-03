b0d542869eaf59a544177cf6fc0a2b9d01bb0755
ayobyary <140202386+ayobyary@users.noreply.github.com>
2025-11-04 00:45:33 +0330

    feat: refactor header component to use standalone desktop and mobile subcomponents for improved structure and maintainability

---
+++ src/app/Components/header/header-desktop/header-desktop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-desktop',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-desktop.component.html',
+  styleUrl: './header-desktop.component.css'
+})
+export class HeaderDesktopComponent {
+
+}
--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderDesktopComponent } from "./header-desktop/header-desktop.component";
+import { HeaderMobileComponent } from "./header-mobile/header-mobile.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  imports: [HeaderDesktopComponent, HeaderMobileComponent]
 })
 export class HeaderComponent {
   
